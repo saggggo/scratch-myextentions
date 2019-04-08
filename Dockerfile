f041af12e24b5320fc06aea16669d82ca2019d06
sago@syun <sago@syun.jp>
2019-04-09 04:23:41 +0900

    sample extension

--- Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM node:lts
-
-EXPOSE 8601
-WORKDIR /code
-ADD ./ /code
-
-RUN ./init.sh
-
-CMD cd ./scratch-gui && npm start
--- docker.sh
+++ docker.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-docker build -t scratch_dev .
-docker run --rm -it -p 8601:8601 -v `pwd`/docs:/code/scratch-gui/build  scratch_dev /bin/bash -c "cd /code/scratch-gui && npm install --save imports-loader && npm run build"
+docker run -it -p 8601:8601 -v `pwd`:/code -v `pwd`/docs:/code/scratch-gui/build node:lts /bin/bash -c "cd /code && ./init.sh && cd /code/scratch-gui && npm install --save imports-loader && npm run build"
